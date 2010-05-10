16f464167814d335a63eb68be29c5054df3dfef5
Michele Bianchi <jazzinghen@gmail.com>
2010-05-10 16:20:26 +0200

    Fixed previous release :3 Finally I've decided to move my project folder to spamADA. The name spamADA comes from Zone Of The Enders 2 (Anyone?), where ADA was the Detached Battle AI for Jehuty. I couldn't find anywhere what does A.D.A. means, so for now, I'll just assume that it means Autonomous Detached AI.

--- spamADA/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload spamADA.rxe...
- /home/jazzinghen/Downloads/NXT\ Dev/bricxcc/NeXTTool /COM=usb -download=spamADA.rxe
- /home/jazzinghen/Downloads/NXT\ Dev/bricxcc/NeXTTool /COM=usb -listfiles=spamADA.rxe
-echo NeXTTool is terminated.
