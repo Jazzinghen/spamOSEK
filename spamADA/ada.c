13dbb244dd9645977bbb3e32baab70b1113c71f3
Michele Bianchi <jazzinghen@gmail.com>
2010-05-10 16:20:00 +0200

    Tons of errors, meh.

--- spamADA/ada.c
+++ spamADA/ada.c
@@ -1,4 +1,8 @@
-/* sonartest.c */ 
+/* ada.c */
+/*
+ * ADA for SPAM, it was time to change that damn directory and project files :3
+ *
+ */ 
 #include "kernel.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
@@ -13,10 +17,14 @@
 #define LOWER_LIMIT 540
 
 /*
- * Defining max SPAM speed.
+ * Defining PID controller's data.
  */
 
-#define TOP_SPEED 85 
+#define P   5           // k in the PID control digital equation
+#define I   2           // k/Ti in the PID control digital equation
+#define D   1           // kTd in the PID control digital equation
+
+#define DELTA_T   0.01  // T is 10ms, in seconds :3
 
 /*
  * Using definitions to speed up test development
@@ -35,23 +43,44 @@
 
 /* OSEK declarations */
 DeclareCounter(SysTimerCnt);
-DeclareTask(Task1);
-DeclareTask(Task2);
-// DeclareTask(DegPSCounter);
+DeclareTask(Sonar_Check);
+DeclareTask(LCD_Update);
+DeclareTask(DPID_Controller);
 
 /* Global Declarations */
 
-U32 degpsreadings[1000];
-int i = 0;
+U32 last_reading = 0;       // Last grad count reading 
+U32 degpsreadings[10];      // Circular array of 10 - 10ms readings
+U32 average_slot = 0;       // What reading in the circular array we should write into 
+U32 speeds[3];              // Last 3 speed readings
+
+U32 motor_power = 0;        // Motors power
+U32 VTEC = 2;               // RPM Value, as in VTEC Just Kicked In, Yo!
 
 U32 getdegpscount() {
   U32 degsum = 0;
   
-  for (int j = 0; j <= 1000; j++) {
+  for (int j = 0; j < 10; j++) {
     degsum += degpsreadings[j];
   }
   
-  return degsum / 1000;
+  return degsum * 10;
+}
+
+U32 DPID_output (U32 last, U32 target_speed) {
+  float Ti = (P / I);
+  float TD = (D / P);
+  float next;
+  
+  /* PID Digital Equation FTW */
+  
+  next = last + P *
+            ( ( ( 1 + ( DELTA_T / Ti ) + ( TD / DELTA_T ) ) * (target_speed - speeds[0] ) ) -
+            ( ( 1 + 2 * ( TD / DELTA_T ) ) * ( target_speed - speeds[1] ) ) +
+            ( ( TD / DELTA_T ) * ( target_speed - speeds[2] ) ) );
+  
+  return (U32)next;
+  
 }
 
 /* 
@@ -96,17 +125,14 @@ void user_1ms_isr_type2(void)
 }
 
 /* Task1 executed every 50msec */
-TASK (Task1) {
+TASK (Sonar_Check) {
 	
   ecrobot_get_sonar_sensor(SONAR_PORT);
 	
  	TerminateTask();
 }
 
-TASK (Task2) {
-
-  nxt_motor_set_speed(RIGHT_MOTOR, 50, 1);
-  nxt_motor_set_speed(LEFT_MOTOR, -50, 1);
+TASK (LCD_Update) {
   
   //ecrobot_status_monitor("Sonar Test");
   display_clear(0);
@@ -121,17 +147,33 @@ TASK (Task2) {
   
   display_goto_xy(0, 3);
   display_string("DegPS:");
-//  display_unsigned(getdegpscount(), 0);
+  display_unsigned(getdegpscount(), 0);
   
   display_update();
   
   TerminateTask();
 }
 
-/*TASK (DegPSCounter){
-  if (i >= 1000) {
-    i = 0;
-  };
-  degpsreadings[i++] = nxt_motor_get_count(RIGHT_MOTOR);
+TASK (DPID_Controller){
+  U32 now;
   
-}*/
+  /* Updating 10ms readings */
+  now = nxt_motor_get_count(RIGHT_MOTOR);
+  degpsreadings[average_slot] = now - last_reading;
+  average_slot = ((average_slot == 9) ? 0 : average_slot+1);
+  last_reading = now;
+  
+  /* Updating speed readings */
+  speeds[2] = speeds[1];
+  speeds[1] = speeds[0];
+  speeds[0] = getdegpscount();
+  
+  /* Get Next Motors' Power */
+  motor_power = DPID_output ( motor_power, (VTEC * 360) );
+  
+  /* Set Motors' Speed */
+  nxt_motor_set_speed(RIGHT_MOTOR, motor_power , 0);
+  
+  TerminateTask();
+  
+}
---
+++ spamADA/rxeflash.sh
@@ -0,0 +1,4 @@
+echo Executing NeXTTool to upload spamADA.rxe...
+ /home/jazzinghen/Downloads/NXT\ Dev/bricxcc/NeXTTool /COM=usb -download=spamADA.rxe
+ /home/jazzinghen/Downloads/NXT\ Dev/bricxcc/NeXTTool /COM=usb -listfiles=spamADA.rxe
+echo NeXTTool is terminated.
